da163f10f9dacab7b7438b669772045bbf4f7d73
usausa9 <k021g1269@g.neec.ac.jp>
2023-07-22 06:01:17 +0900

    コメントアウト戻し忘れ修正 最新版

--- enc_temp_folder/3567d59c753cd44f016a12b0c27dd8/Player.cpp
+++ enc_temp_folder/3567d59c753cd44f016a12b0c27dd8/Player.cpp
@@ -85,10 +85,10 @@ void Player::reticleUpdate()
 	// ビューポート行列
 	Matrix4 matViewPort;
 	matViewPort.Identity();
-	matViewPort.m[0][0] =   WinAPI::Get()->width  / 2.0f;
+	/*matViewPort.m[0][0] =   WinAPI::Get()->width  / 2.0f;
 	matViewPort.m[1][1] = -(WinAPI::Get()->height / 2.0f);
 	matViewPort.m[3][0] =   WinAPI::Get()->width  / 2.0f;
-	matViewPort.m[3][1] =   WinAPI::Get()->height / 2.0f;
+	matViewPort.m[3][1] =   WinAPI::Get()->height / 2.0f;*/
 
 	// カメラ行列との合成
 	Matrix4 matViewProjectionViewPort = 
